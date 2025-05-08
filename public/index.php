edbdd5c67cf7706e4b9327aeb27bbb9181e2a058
devjuju <jleleubellpro1994@gmail.com>
2025-05-08 18:47:43 +0200

    set up public folder

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+// to display the error message
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+//require "src/core/router.php";
+require "../vendor/autoload.php";
